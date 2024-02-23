549e232bf225317b11c79fe7429dc15a6b082ad8
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-02-23 17:41:37 +0900

    :tada: init

---
+++ src/main/java/com/sns/room/user/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.sns.room.user.controller;
+
+public class UserController {
+
+}
---
+++ src/main/java/com/sns/room/user/entity/User.java
@@ -0,0 +1,37 @@
+package com.sns.room.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String introduce;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+
+}
