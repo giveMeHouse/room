d9eb284ae5a3e515b2212e1ba545b5feb028dd37
이건희 <leebiki@gmail.com>
2024-02-23 21:11:14 +0900

    jwt Token

---
+++ src/main/java/com/sns/room/user/jwt/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.sns.room.user.jwt;
+
+import com.sns.room.user.repository.UserRepository;
+import com.sns.room.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+		return new UserDetailsImpl(user);
+	}
+}
