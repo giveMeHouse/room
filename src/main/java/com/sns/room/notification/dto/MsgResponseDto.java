60e6bbe19c72fc3d242f35404c82bef38ad83f33
wooseok <wooseok6008@gmail.com>
2024-02-27 17:02:06 +0900

    :sparkles: feat(Notification): 댓글 생성시 실시간 알림 구현

---
+++ src/main/java/com/sns/room/notification/dto/MsgResponseDto.java
@@ -0,0 +1,11 @@
+package com.sns.room.notification.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MsgResponseDto {
+
+    private String message;
+    private int code;
+
+}
--- src/main/java/com/sns/room/notification/service/NotificationService.java
+++ src/main/java/com/sns/room/notification/service/NotificationService.java
@@ -1,7 +1,15 @@
 package com.sns.room.notification.service;
 
+import com.sns.room.comment.entity.Comment;
+import com.sns.room.comment.repository.CommentRepository;
 import com.sns.room.notification.controller.NotificationController;
+import com.sns.room.notification.entity.Notification;
+import com.sns.room.notification.repository.NotificationRepository;
+import com.sns.room.post.entity.Post;
+import com.sns.room.post.service.PostService;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -9,6 +17,12 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final PostService postService;
+    private final CommentRepository commentRepository;
+
+    private static Map<Long, Integer> notificationCounts = new HashMap<>();
 
     public SseEmitter subscribe(Long userId) {
         // 현재 클라이언트를 위한 sseEmitter 생성
@@ -29,5 +43,46 @@ public class NotificationService {
         sseEmitter.onError((e) -> NotificationController.sseEmitters.remove(userId));
 
         return sseEmitter;
+    }
+
+    public void notifyComment(Long postId) {
+        Post post = postService.findPost(postId);
+
+        Comment receiveComment = commentRepository.findFirstByPostIdOrderByCreatedAtDesc(
+            post.getId()).orElseThrow(
+            () -> new IllegalArgumentException("댓글을 찾을 수 없습니다.")
+        );
+
+        Long userId = post.getUser().getId();
+
+        if (NotificationController.sseEmitters.containsKey(userId)) {
+            SseEmitter sseEmitter = NotificationController.sseEmitters.get(userId);
+            try {
+                Map<String, String> eventData = new HashMap<>();
+                eventData.put("sender", receiveComment.getUser().getUsername());
+                eventData.put("contents", receiveComment.getComment());
+                eventData.put("createdAt", receiveComment.getCreatedAt().toString());
+
+                sseEmitter.send(SseEmitter.event().name("addComment").data(eventData));
+
+                // DB 저장
+                Notification notification = new Notification();
+                notification.setSender(receiveComment.getUser().getUsername());
+                notification.setContents(receiveComment.getComment());
+                notification.setCreatedAt(receiveComment.getCreatedAt());
+                notification.setPost(post);
+                notificationRepository.save(notification);
+
+                // 알림 개수 증가
+                notificationCounts.put(userId, notificationCounts.getOrDefault(userId, 0) + 1);
+
+                // 현재 알림 개수 전송
+                sseEmitter.send(SseEmitter.event().name("notificationCount")
+                    .data(notificationCounts.get(userId)));
+
+            } catch (IOException e) {
+                NotificationController.sseEmitters.remove(userId);
+            }
+        }
     }
 }
