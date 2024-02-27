f510e8582db9b95cbb6bc2c3614b5dd144bce814
이건희 <leebiki@gmail.com>
2024-02-27 16:23:43 +0900

    testcode

---
+++ src/test/java/com/sns/room/user/controller/AuthControllerTest.java
@@ -0,0 +1,16 @@
+package com.sns.room.user.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AuthControllerTest {
+
+	@Test
+	void signup() {
+	}
+
+	@Test
+	void login() {
+	}
+}
