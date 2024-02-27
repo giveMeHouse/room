5d5cb810e3d55e7f847fc913c4480cb002cc9cb4
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-02-27 19:59:28 +0900

    :white_check_mark: PasswordUpdateRequestDto 테스트 코드 작성

--- src/test/java/com/sns/room/user/dto/PasswordUpdateRequestDtoTest.java
+++ src/test/java/com/sns/room/user/dto/PasswordUpdateRequestDtoTest.java
@@ -1,8 +1,29 @@
 package com.sns.room.user.dto;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PasswordUpdateRequestDtoTest {
+
+    @Test
+    @DisplayName("PasswordUpdateRequestDto 생성 및 필드 검증")
+    void createPasswordUpdateRequestDto_Test(){
+        // Given
+        String password = "currentPassword";
+        String changePassword = "newPassword";
+        String changePasswordCheck = "newPassword";
+
+        // When
+        PasswordUpdateRequestDto passwordUpdateRequestDto = new PasswordUpdateRequestDto(password, changePassword, changePasswordCheck);
+
+        // Then
+        assertThat(passwordUpdateRequestDto.getPassword()).isEqualTo(password);
+        assertThat(passwordUpdateRequestDto.getChangePassword()).isEqualTo(changePassword);
+        assertThat(passwordUpdateRequestDto.getChangePasswordCheck()).isEqualTo(changePasswordCheck);
+    }
 
 }
