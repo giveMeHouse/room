e2371e6475fdb02919ab2a621038bae5a19c6660
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-02-28 21:48:19 +0900

    :white_check_mark: add test code. (#16)
    * :tada: init
    
    * :sparkles: 프로필 조회 기능
    
    * :sparkles: 프로필 조회 기능
    
    * :sparkles: 프로필 수정 기능
    
    * :sparkles: 비밀번호 변경 기능
    
    * :recycle: 프로필 조회 기능 코드 수정
    
    * :white_check_mark: 테스트 코드 작성
    
    * :white_check_mark: PasswordUpdateRequestDto 테스트 코드 작성
    
    * :white_check_mark: add test code.
    
    * :recycle: response 메세지 삭제
    
    * :recycle: setter 삭제
    
    * :recycle: user entity 생성자 id 삭제
    
    * :white_check_mark: add test code.

--- src/main/java/com/sns/room/user/entity/User.java
+++ src/main/java/com/sns/room/user/entity/User.java
@@ -15,8 +15,12 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Getter
@@ -40,12 +44,21 @@ public class User extends Timestamped {
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
+    @Column
+    private String introduce;
 
-//		@Column
-//		private String introduce;
 //
 //		@Column(nullable = false)
 //		private LocalDateTime createAt;
+
+    @Builder
+    public User(String username, String email, String password, UserRoleEnum role, String introduce) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.introduce = introduce;
+    }
 
     public User(String username, String email, String password, UserRoleEnum role) {
         this.username = username;
@@ -53,4 +66,14 @@ public class User extends Timestamped {
         this.password = password;
         this.role = role;
     }
+
+    public void update(String username, String introduce) {
+        this.username = username;
+        this.introduce = introduce;
+    }
+
+    public void updatePassword(String newPassword, PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(newPassword);
+    }
+
 }
--- src/test/java/com/sns/room/user/controller/AuthControllerTest.java
+++ src/test/java/com/sns/room/user/controller/AuthControllerTest.java
@@ -1,12 +1,25 @@
 package com.sns.room.user.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sns.room.global.jwt.UserDetailsImpl;
 import com.sns.room.user.dto.LoginRequestDto;
+import com.sns.room.user.dto.PasswordUpdateRequestDto;
 import com.sns.room.user.dto.ResponseDto;
 import com.sns.room.user.dto.SignupRequestDto;
+import com.sns.room.user.dto.UserRequestDto;
+import com.sns.room.user.dto.UserResponseDto;
+import com.sns.room.user.entity.User;
 import com.sns.room.user.entity.UserRoleEnum;
 import com.sns.room.user.service.AuthService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,27 +27,61 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
-class AuthControllerTest {
 
-	@Mock
-	AuthService authService;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 	@InjectMocks
 	private AuthController authController;
-	@BeforeEach
-	void setUp() {
+
+    private ObjectMapper objectMapper;
+
+    private UserDetailsImpl userDetailsImpl;
+    private UserRequestDto userRequestDto;
+    private UserResponseDto userResponseDto;
+	private AuthService authService;
+
+    @BeforeEach
+    void setUp() {
 		authService = mock(AuthService.class);
 		authController = new AuthController(authService);
-	}
+
+        objectMapper = new ObjectMapper();
+        User user = User.builder()
+            .username("testUser")
+            .password("TestPassword")
+            .introduce("Test introduction")
+            .role(UserRoleEnum.USER)
+            .build();
+
+        this.userDetailsImpl = new UserDetailsImpl(user);
+
+        this.userRequestDto = new UserRequestDto();
+        userRequestDto.setUsername("newTestUser");
+        userRequestDto.setIntroduce("New Test introduction");
+
+        this.userResponseDto = new UserResponseDto(user);
+    }
 
 	@Test
 	void signup() {
@@ -88,5 +135,48 @@ class AuthControllerTest {
 
 		// then
 		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+	}
+
+	@Test
+	@WithMockUser
+	void getUserProfile_Success() throws Exception {
+
+		given(authService.getUserProfile(userDetailsImpl.getUser().getId())).willReturn(userResponseDto);
+
+		mockMvc.perform(get("/api/auth/mypage").with(user(userDetailsImpl)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data.username").value("testUser"))
+			.andExpect(jsonPath("$.data.introduce").value("Test introduction"));
+	}
+
+	@Test
+	void updateUser_Success() throws Exception {
+
+		given(authService.updateUser(any(UserDetailsImpl.class), any(UserRequestDto.class))).willReturn(userResponseDto);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String userRequestDtoJson = objectMapper.writeValueAsString(userRequestDto);
+
+		mockMvc.perform(put("/api/auth/mypage")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userRequestDtoJson)
+				.with(user(userDetailsImpl)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data.username").value("testUser"))
+			.andExpect(jsonPath("$.data.introduce").value("Test introduction"));
+	}
+
+	@Test
+	@WithMockUser
+	void updatePassword_Success() throws Exception {
+		PasswordUpdateRequestDto passwordUpdateRequestDto = new PasswordUpdateRequestDto("currentPassword", "newPassword", "newPassword");
+
+		doNothing().when(authService).updatePassword(any(UserDetailsImpl.class), any(PasswordUpdateRequestDto.class));
+
+		mockMvc.perform(put("/api/auth/password-patch")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(passwordUpdateRequestDto))
+				.with(user(userDetailsImpl.getUsername())))
+			.andExpect(status().isOk());
 	}
 }
