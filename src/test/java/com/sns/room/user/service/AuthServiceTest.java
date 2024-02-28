9d605ea7a4f838e0aeac58aef9b75289a00c124e
이건희 <leebiki@gmail.com>
2024-02-28 17:29:10 +0900

    controller, service-login test code

--- src/test/java/com/sns/room/user/service/AuthServiceTest.java
+++ src/test/java/com/sns/room/user/service/AuthServiceTest.java
@@ -2,9 +2,13 @@ package com.sns.room.user.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.sns.room.global.jwt.JwtUtil;
 import com.sns.room.user.dto.LoginRequestDto;
 import com.sns.room.user.dto.SignupRequestDto;
 import com.sns.room.user.entity.User;
@@ -23,66 +27,81 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
-@SpringBootTest
+
 @ExtendWith(MockitoExtension.class)
 
 class AuthServiceTest {
 
-	@Autowired
-	private AuthService authService;
-	@MockBean
+	@Mock
 	private UserRepository userRepository;
+
+	@Mock
+	private JwtUtil jwtUtil;
+
 	@Mock
 	private PasswordEncoder passwordEncoder;
+
+	@InjectMocks
+	private AuthService authService;
+
+	@Mock
+	private HttpServletResponse response;
 
 	@Test
 	@DisplayName("회원가입")
 	void signup() {
-		SignupRequestDto dto = new SignupRequestDto();
-		ReflectionTestUtils.setField(dto, "username", "testUser");
-		ReflectionTestUtils.setField(dto, "email", "test@example.com");
-		ReflectionTestUtils.setField(dto, "password","testPassword");
-		ReflectionTestUtils.setField(dto, "role", UserRoleEnum.USER);
-
-		User user = new User(dto.getUsername(),dto.getEmail(),dto.getPassword(),dto.getRole());
-
-		when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
-		when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.empty());
-		when(passwordEncoder.encode(user.getPassword())).thenReturn("password");
-
-		userRepository.save(user);
-		authService.signup(dto);
-
-		assertEquals(1,userRepository.count());
 	}
 
 	@Test
-	void loginUsername() {
+	void loginTest() {
+		// given
+		LoginRequestDto dto = new LoginRequestDto();
+		dto.setUsername("testUser");
+		dto.setPassword("testPassword");
+
+		User user = new User();
+		ReflectionTestUtils.setField(user, "username", "testUser");
+		ReflectionTestUtils.setField(user, "password","encodePassword");
+		ReflectionTestUtils.setField(user, "role", UserRoleEnum.USER);
+
+		given(userRepository.findByUsername("testUser")).willReturn(java.util.Optional.of(user));
+		given(passwordEncoder.matches("testPassword", "encodePassword")).willReturn(true);
+		given(jwtUtil.createToken("testUser", UserRoleEnum.USER)).willReturn("Token");
+
+		// when
+		authService.login(dto, response);
+
+		// then
+		verify(userRepository, times(1)).findByUsername("testUser");
+		verify(passwordEncoder, times(1)).matches("testPassword", "encodePassword");
+	}
+
+	@Test
+	void Username() {
+		// given
 		LoginRequestDto loginRequestDto = new LoginRequestDto("testUsername", "testPassword");
-		HttpServletResponse response = mock(HttpServletResponse.class);
 
 		when(userRepository.findByUsername(loginRequestDto.getUsername())).thenReturn(Optional.empty());
 
+		// when
 		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
 			authService.login(loginRequestDto, response);
 		});
 
+		// then
 		assertEquals("존재하지 않는 username입니다.", exception.getMessage());
 	}
 
 	@Test
-	void loginPassword() {
+	void Password() {
 		// Given
 		String username = "testUsername";
 		String password = "5000";
 		LoginRequestDto loginRequestDto = new LoginRequestDto(username, password);
 
-		HttpServletResponse response = mock(HttpServletResponse.class);
-
 		User user = new User();
 		ReflectionTestUtils.setField(user, "username", "testUsername");
 		ReflectionTestUtils.setField(user, "password", "5050");
-
 
 		when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
 		when(passwordEncoder.matches(password, user.getPassword())).thenReturn(false);
